66ab2a002cc187331f4c30d560d7b3954473c15a
Jeff Gerlach <jeffgerlach06@gmail.com>
2018-03-30 14:07:21 -0600

    Add form input to change name

--- src/App.js
+++ src/App.js
@@ -1,14 +1,46 @@
-import React, { Component } from 'react';
-import './App.css';
-import Person from './Person/Person';
+import React, { Component } from "react";
+import "./App.css";
+import Person from "./Person/Person";
 
 class App extends Component {
+  state = {
+    cats: [{ name: "Sox", age: 10 }, { name: "Babby", age: 1 }],
+    otherState: "some other value"
+  };
+
+  switchNameHandler = newName => {
+    // console.log('Was clicked!');
+    this.setState({
+      cats: [{ name: newName, age: 10 }, { name: "Babby", age: 1.5 }]
+    });
+  };
+
+  nameChangedHandler = event => {
+    this.setState({
+      cats: [{ name: "Sox", age: 10 }, { name: event.target.value, age: 1 }]
+    });
+  };
+
   render() {
     return (
       <div className="App">
         <h1>Hi, I'm a React app!</h1>
         <p>This is really working!</p>
-        <Person name="Sox" age="10">My Hobbies: Eating</Person>
+        {/*  arrow function can be inefficient */}
+        <button onClick={() => this.switchNameHandler("Pooter")}>
+          Switch Name
+        </button>
+        <Person
+          name={this.state.cats[0].name}
+          age={this.state.cats[0].age}
+          click={this.switchNameHandler.bind(this, "Sox!")}
+        >
+          My Hobbies: Eating
+        </Person>
+        <Person 
+          name={this.state.cats[1].name}
+          age={this.state.cats[1].age}
+          changed={this.nameChangedHandler}/>
       </div>
     );
   }

63fa3f328931f1c2378a4d523beec0a2e5ab89a8
Jeff Gerlach <jeffgerlach06@gmail.com>
2018-04-01 13:12:56 -0600

    Add stylesheet for Person component. Conditionally render Persons list based on toggle switch.

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React, { Component } from "react";
 import "./App.css";
 import Person from "./Person/Person";
+import person from "./Person/Person";
 
 class App extends Component {
   state = {
     cats: [{ name: "Sox", age: 10 }, { name: "Babby", age: 1 }],
-    otherState: "some other value"
+    otherState: "some other value",
+    showPersons: false
   };
 
   switchNameHandler = newName => {
@@ -21,6 +23,11 @@ class App extends Component {
     });
   };
 
+  togglePersonsHandler = () => {
+    const doesShow = this.state.showPersons;
+    this.setState({showPersons: !doesShow});
+  }
+
   render() {
     const style = {
       backgroundColor: "white",
@@ -29,27 +36,32 @@ class App extends Component {
       padding: "8px",
       cursor: "pointer"
     };
+    const persons = (
+      <div>
+          <Person
+            name={this.state.cats[0].name}
+            age={this.state.cats[0].age}
+            click={this.switchNameHandler.bind(this, "Sox!")}
+          >
+            My Hobbies: Eating
+          </Person>
+          <Person
+            name={this.state.cats[1].name}
+            age={this.state.cats[1].age}
+            changed={this.nameChangedHandler}
+          />
+        </div>
+    );
 
     return (
       <div className="App">
         <h1>Hi, I'm a React app!</h1>
         <p>This is really working!</p>
         {/*  arrow function can be inefficient */}
-        <button style={style} onClick={() => this.switchNameHandler("Pooter")}>
-          Switch Name
+        <button style={style} onClick={this.togglePersonsHandler}>
+          Show Names
         </button>
-        <Person
-          name={this.state.cats[0].name}
-          age={this.state.cats[0].age}
-          click={this.switchNameHandler.bind(this, "Sox!")}
-        >
-          My Hobbies: Eating
-        </Person>
-        <Person
-          name={this.state.cats[1].name}
-          age={this.state.cats[1].age}
-          changed={this.nameChangedHandler}
-        />
+        { this.state.showPersons && persons }
       </div>
     );
   }

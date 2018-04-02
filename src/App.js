9e2b5904fa1da667f4a280c8ede508f9c37148c4
Jeff Gerlach <jgerlach@tmcdesign.com>
2018-04-02 13:56:11 -0600

    Use imported css modules in App component

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from "react";
-import "./App.css";
+import classes from "./App.css";
 import Cat from "./Cat/Cat";
 
 class App extends Component {
@@ -66,16 +66,16 @@ class App extends Component {
       style.backgroundColor = "red";
     }
 
-    const classes = [];
+    const assignedClasses = [];
     if (this.state.cats.length <= 2) {
-      classes.push("red");
+      assignedClasses.push(classes.red);
     }
     if (this.state.cats.length <= 1) {
-      classes.push("bold");
+      assignedClasses.push(classes.bold);
     }
 
     return (
-      <div className="App">
+      <div className={classes.App}>
         <h1>Hi, I'm a React app!</h1>
         <p className={classes.join(" ")}>This is really working!</p>
         {/*  arrow function can be inefficient */}
